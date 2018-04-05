49d1b9a8e1291ca36d4130bded3480c2ad67bd46
sdadn <38024451+sdadn@users.noreply.github.com>
2018-04-05 11:37:15 -0500

    Added Wifi Capability to Hub

--- iOS_comm_app1/iOS_comm_app1/Main.cs
+++ iOS_comm_app1/iOS_comm_app1/Main.cs
@@ -7,6 +7,7 @@ namespace iOS_comm_app1
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
