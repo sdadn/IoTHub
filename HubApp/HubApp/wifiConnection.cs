f96edc2e19309ed82a6c026b2974e75d95fa4d63
sdadn <38024451+sdadn@users.noreply.github.com>
2018-04-21 13:32:07 -0500

    added wifif class

---
+++ HubApp/HubApp/wifiConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.WiFi;
+using Windows.UI.Popups;
+
+namespace HubApp
+{
+    class wifiConnection
+    {
+        bool adapter_access;
+
+
+        public bool hasAccess
+        {
+            get { return this.adapter_access; }
+        }
+
+        
+
+        public async void test_access()
+        {
+            adapter_access = false;
+
+            var access = await WiFiAdapter.RequestAccessAsync();
+
+            if (access != WiFiAccessStatus.Allowed)
+            {
+                MessageDialog msg = new MessageDialog("No adapter access.");
+                await msg.ShowAsync();
+                return;
+            }
+
+            adapter_access = true;
+        }
+
+
+    }
+}
