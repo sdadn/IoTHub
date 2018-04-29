7b7fdb48e01d195b85ca67d61f9d770ca65127b5
sdadna <saadadnan94@gmail.com>
2018-04-29 16:43:37 -0500

    continued work on db

--- HubLibrary/HubLibrary/DataAccess.cs
+++ HubLibrary/HubLibrary/DataAccess.cs
@@ -10,32 +10,90 @@ namespace HubLibrary
 {
     public static class DataAccess
     {
-        public static void InitializeDB_HUB()
-        {
-            using (SqliteConnection db = new SqliteConnection("Filename=hub.db"))
+        public static class Hub{
+
+            public static void InitializeDB_HUB()
             {
-                db.Open();
+                static filename ="Filename=hub.db";
 
-                string init_DeviceTable = "CREATE TABLE IF NOT EXISTS " +
-                                          "Devices ( Primary_Key INTEGER PRIMARY KEY AUTOINCREMENT, DeviceID INTEGER,  DeviceName NVARCHAR(20) NULL)";
+                using (SqliteConnection db = new SqliteConnection(filename))
+                {
+                    db.Open();
+
+                    string init_DeviceTable =   "CREATE TABLE IF NOT EXISTS Devices ( " +
+                                                "DeviceID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                "DeviceSR TEXT, " +
+                                                "DeviceName NVARCHAR(20) NULL)";
+
+                    string init_UserTable = "CREATE TABLE IF NOT EXISTS Users ( " +
+                                            "UserID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                            "Username NVARCHAR(20),  " +
+                                            "Password NVARCHAR(40), IsAdmin INTEGER, Cert Text)";
+
+                    new SqliteCommand(init_DeviceTable, db).ExecuteReader();
+                    new SqliteCommand(init_UserTable, db).ExecuteReader();
+
+                    db.Close();
+                }
+            }
+
+            public static void AddAdmin(string username, string Password)
+            {
+
+            }
+
+            public static bool CheckAdmin(string username)
+            {
+                using (SqliteConnection db = new SqliteConnection(filename))
+                {
+                    db.Open();
+
+                    string cmd = "SELECT * from Users where IsAdmin = \'1\'";
 
 
-                //db.Close();
+                    SqliteDataReader query = new SqliteCommand(init_DeviceTable, db).ExecuteReader();
+
+  
+
+                    db.Close();
+                }
+            }
+
+        }
+
+
+        public static class Win{
+
+            public static void InitializeDB_WIN()
+            {
+
             }
         }
-        public static void InitializeDB_WIN()
-        {
 
+
+        public static class Device{
+
+            public static void InitializeDB_DEVICE()
+            {
+                using (SqliteConnection db = new SqliteConnection("Filename=device.db"))
+                {
+                    db.Open();
+
+                    string init_DeviceTable = "CREATE TABLE IF NOT EXISTS " +
+                                            "Devices ( DeviceID INTEGER PRIMARY KEY AUTOINCREMENT, DeviceSR TEXT, DeviceName NVARCHAR(20) NULL)";
+
+                    string init_UserTable = "CREATE TABLE IF NOT EXISTS " +
+                                            "Users ( UserID INTEGER PRIMARY KEY AUTOINCREMENT, Username NVARCHAR(20),  Password NVARCHAR(40), IsAdmin INTEGER, Cert Text)";
+
+                    new SqliteCommand(init_DeviceTable, db).ExecuteReader();
+                    new SqliteCommand(init_UserTable, db).ExecuteReader();
+
+                    db.Close();
+                }
+
+            }
         }
-        public static void InitializeDB_DEVICE()
-        {
 
-        }
-
-        public static void AddAdmin()
-        {
-
-        }
 
     }
 
